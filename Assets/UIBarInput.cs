df18f03dfb77dbb91660d7263c29a75ed0a4d27e
SherwynSin <sherwyn_sin96@hotmail.com>
2015-05-26 09:37:51 +0800

    Update (26/5/15)

---
+++ Assets/UIBarInput.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIBarInput : MonoBehaviour {
+
+	public GUITexture StartButton;
+
+	void Update()
+	{
+		if(StartButton.HitTest(Input.mousePosition))
+		{
+				
+						Application.LoadLevel (1);
+				
+		}
+	}
+
+}
